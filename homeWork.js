82f40905b7a36fff6ed0364eabfe77b464c620eb
Emre Akinci <emreakinci97@hotmail.com>
2022-03-19 19:04:16 +0300

    deneme

--- homeWork.js
+++ homeWork.js
@@ -1,14 +1,16 @@
-let yourName = prompt("What is your name?")
+var yourName = prompt("What is your name?")
 const date = new Date()
 let today
 const todayDay = date.getDate()
 const todayMonth = date.getMonth()
 const todayYear = date.getFullYear()
 
-let nameInfo = document.querySelector("#myName")
+let nameInfo = document.querySelector
+("#myName")
+let todayHtml =document.querySelector("#myClock")
 nameInfo.innerHTML = yourName.toString
 
 today = todayDay.toString + "." + todayMonthtoString + "." + todayYeartoString
 
-let todayHtml =document.querySelector("#myClock")
+
 todayHtml.innerHTML = today
